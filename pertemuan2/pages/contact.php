5e94bb352834971e238ea7b5ff8da38fa93c677b
Far <37771864+stdarkpha@users.noreply.github.com>
2024-10-12 14:55:55 +0700

    latest adjusment

--- pertemuan2/pages/contact.php
+++ pertemuan2/pages/contact.php
@@ -1,35 +1,80 @@
-<div class="max-w-sm mx-auto py-16">
-    <?php if (isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['message'])): ?>
-        <?php
-        $email = $_POST['email'];
-        $phone = $_POST['phone'];
-        $message = $_POST['message'];
-        ?>
+<div class="max-w-screen-sm mx-auto py-16">
+    <?php if (isset($_POST['submit'])): ?>
         <div class="text-center">
-
-            <h1 class="text-2xl font-semibold">Thank you for Contacting us!</h1>
-            <p class="mb-4">Here is the information you submitted:</p>
+            <h1 class="text-2xl font-semibold">
+                Terima kasih <?= $_POST['name'] ?>! Telah menghubungi saya.
+            </h1>
+            <p class="mb-4">
+                Berikut adalah data yang kamu masukkan:
+            </p>
             <ul class="text-left">
-                <li>Email: <?= $email ?></li>
-                <li>Phone: <?= $phone ?></li>
-                <li>Message: <?= $message ?></li>
+                <li>Nama: <?= $_POST['name'] ?? '' ?></li>
+                <li>Telepon: <?= $_POST['phone'] ?? '' ?></li>
+                <li>Tanggal Lahir: <?= $_POST['birth'] ?? '' ?></li>
+                <li>Jenis Kelamin: <?= $_POST['gender'] ?? '' ?></li>
+                <li>Hobby: <?= isset($_POST['hobby']) ? implode(', ', (array)$_POST['hobby']) : '' ?></li>
+                <li>Pesan: <?= $_POST['message'] ?></li>
             </ul>
         </div>
-        <a href="index.php?page=contact" class="w-full mt-8 btn btn-primary">Back</a>
+        <a href="index.php?page=contact" class="w-full mt-8 btn btn-primary">Kembali</a>
     <?php else: ?>
-        <h1 class="text-2xl font-semibold">Contact Us</h1>
-        <p class="mb-4">Fill out the form below to contact us.</p>
-        <form method="POST" action="index.php?page=contact" class="flex flex-col gap-4">
-            <label class="input input-bordered flex items-center gap-2">
+        <h1 class="text-2xl font-semibold text-center mb-2">Kontak Saya</h1>
+        <p class="mb-8 text-center">
+            Silahkan isi form di bawah ini untuk menghubungi saya. Saya akan segera merespon pesan Anda.
+        </p>
+        <form method="POST" action="index.php?page=contact" class="flex flex-col gap-6">
+            <div class="flex gap-4 items-center w-full">
+                <label class="w-full input input-bordered flex items-center gap-2">
+                    <input name="name" type="text" class="grow" placeholder="Nama" />
+                </label>
+                <label class="w-full input input-bordered flex items-center gap-2">
+                    <input name="phone" type="text" class="grow" placeholder="Telepon" />
+                </label>
+            </div>
 
-                <input required name="email" type="text" class="grow" placeholder="Email" />
-            </label>
-            <label class="input input-bordered flex items-center gap-2">
+            <div class="w-full">
+                <p class="mb-2">Tanggal Lahir</p>
+                <label class="input input-bordered flex items-center gap-2">
+                    <input name="birth" type="date" class="grow" />
+                </label>
+            </div>
 
-                <input required name="phone" type="text" class="grow" placeholder="Phone" />
-            </label>
-            <textarea required name="message" class="textarea textarea-bordered" placeholder="Messages"></textarea>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <div class="flex gap-4 items-center w-full">
+                <div class="w-full">
+                    <p class="mb-2">Jenis Kelamin</p>
+                    <div class="flex items-center gap-4">
+                        <label class="flex items-center gap-2">
+                            <input type="radio" name="gender" value="laki-laki" class="radio w-4 h-4" />
+                            <span>Laki-Laki</span>
+                        </label>
+                        <label class="flex items-center gap-2">
+                            <input type="radio" name="gender" value="perempuan" class="radio w-4 h-4" />
+                            <span>Perempuan</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="w-full">
+                    <p class="mb-2">Pilih Hobby</p>
+                    <div class="flex gap-4">
+                        <label class="flex items-center gap-2">
+                            <input type="checkbox" name="hobby[]" class="checkbox w-4 h-4" value="lari" />
+                            <span>Lari</span>
+                        </label>
+                        <label class="flex items-center gap-2">
+                            <input type="checkbox" name="hobby[]" class="checkbox w-4 h-4" value="renang" />
+                            <span>Renang</span>
+                        </label>
+                        <label class="flex items-center gap-2">
+                            <input type="checkbox" name="hobby[]" class="checkbox w-4 h-4" value="makan" />
+                            <span>Makan</span>
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            <textarea name="message" class="textarea textarea-bordered" placeholder="Pesan.."></textarea>
+
+            <button name="submit" type="submit" class="btn btn-primary">Kirim</button>
         </form>
 
     <?php endif; ?>
